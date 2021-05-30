cf8a35696ec10698deb5b63ac4d7461e0d1f104a
sailae <saila.e.nieminen@gmail.com>
2021-05-30 15:22:18 +0300

    added conditional formating

--- map/src/MapChart.js
+++ map/src/MapChart.js
@@ -1,17 +1,15 @@
-import React, { memo } from "react";
+import React from "react";
 import {
   ComposableMap,
   Geographies,
   Geography
 } from "react-simple-maps";
-import { useHistory } from "react-router-dom";
 import allStates from "./data.json";
 
 const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
-const MapChart = ({ setTooltipContent }) => {
-  const history = useHistory();
+const MapChart = () => {
   return (
     <ComposableMap
       projection="geoAzimuthalEqualArea"
@@ -29,26 +27,31 @@ const MapChart = ({ setTooltipContent }) => {
               <Geography
                 key={geo.rsmKey}
                 geography={geo}
-                fill="#fff"
-                stroke="#8BBE86"
+                fill="#8BBE86"
+                stroke="#72A46D"
+                cursor="pointer"
                 onClick={(e) => {
-                  e.preventDefault();
-                  window.location.href=cur.href;
+                  if (cur !== undefined) {
+                    e.preventDefault();
+                    window.location.href=cur.href;
+                  }
                 }}
                 style={{
                   default: {
-                    fill: "#D6D6DA",
+                    fill: cur !== undefined? 'fill': '#EDEAE5',
+                    stroke: cur !== undefined? 'stroke': '#E3DFD9',
                     outline: "none"
                   },
                   hover: {
-                    fill: "#8BBE86",
+                    fill: cur !== undefined? 'fill': '#EDEAE5',
+                    stroke: cur !== undefined? 'stroke': '#E3DFD9',
                     outline: "none",
-                    cursor: "pointer"
+                    cursor: cur !== undefined? 'cursor': "default"
                   },
                   pressed: {
-                    fill: "#F79320",
-                    outline: "#F79320",
-                    stroke: "#F79320"
+                    fill: cur !== undefined? 'fill': '#EDEAE5',
+                    outline: "none",
+                    stroke: cur !== undefined? 'stroke': '#E3DFD9'
                   }
                 }}
               />
