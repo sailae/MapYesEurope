1b971cacf3a6cadee300e7cca38af20f5a3dcab9
sailae <saila.e.nieminen@gmail.com>
2021-05-30 13:00:48 +0300

    added hover elements

--- map/src/MapChart.js
+++ map/src/MapChart.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { memo } from "react";
 import {
+  ZoomableGroup,
   ComposableMap,
   Geographies,
   Geography,
@@ -9,7 +10,17 @@ import {
 const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
-const MapChart = () => {
+  const rounded = num => {
+    if (num > 1000000000) {
+      return Math.round(num / 100000000) / 10 + "Bn";
+    } else if (num > 1000000) {
+      return Math.round(num / 100000) / 10 + "M";
+    } else {
+      return Math.round(num / 100) / 10 + "K";
+    }
+  };
+
+const MapChart = ({ setTooltipContent }) => {
   return (
     <ComposableMap
       projection="geoAzimuthalEqualArea"
@@ -19,18 +30,41 @@ const MapChart = () => {
       }}
     >
       <Graticule display="none" />
-      <Geographies geography={geoUrl}>
-        {({ geographies }) =>
-          geographies.map(geo => (
-            <Geography
-              key={geo.rsmKey}
-              geography={geo}
-              fill="#fff"
-              stroke="#8BBE86"
-            />
-          ))
-        }
-      </Geographies>
+      <ZoomableGroup>
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map(geo => (
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                fill="#fff"
+                stroke="#8BBE86"
+                onMouseEnter={() => {
+                  const { NAME, POP_EST } = geo.properties;
+                  setTooltipContent(`${NAME} — ${rounded(POP_EST)}`);
+                }}
+                onMouseLeave={() => {
+                  setTooltipContent("");
+                }}
+                style={{
+                  default: {
+                    fill: "#D6D6DA",
+                    outline: "none"
+                  },
+                  hover: {
+                    fill: "#F53",
+                    outline: "none"
+                  },
+                  pressed: {
+                    fill: "#E42",
+                    outline: "none"
+                  }
+                }}
+              />
+            ))
+          }
+        </Geographies>
+      </ZoomableGroup>
     </ComposableMap>
   );
 };
--- map/src/index.js
+++ map/src/index.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import ReactTooltip from "react-tooltip";
 
 import "./styles.css";
 
 import MapChart from "./MapChart";
 
 function App() {
+  const [content, setContent] = useState("");
   return (
     <div>
-      <MapChart />
+      <MapChart setTooltipContent={setContent} />
+      <ReactTooltip>{content}</ReactTooltip>
     </div>
   );
 }
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+
